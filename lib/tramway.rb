974e6c12be40aba5096f7c49bf0c59cfacd21b49
Rafi Gemhar <gemhar@zoho.com>
2022-09-14 08:05:18 +0530

    1.b bundled gem tramway

--- lib/tramway.rb
+++ lib/tramway.rb
@@ -7,6 +7,7 @@ module Tramway
   # Your code goes here...
   class Application
     def call(env)
+      `echo debug > debug.txt`
       [200,
       {"Content-Type" => "text/html"},
       ["Hello, world from tramway!!!"]]
